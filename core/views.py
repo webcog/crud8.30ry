4624f8a25def018e3e5e296a1d8e056afd01a503
webcog <wajahatmurtaza144@gmail.com>
2023-11-14 20:28:30 +0500

    up

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
+# import djngo
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello this is home page in django")
+
+
+def about(request):
+    return HttpResponse("About Us")
